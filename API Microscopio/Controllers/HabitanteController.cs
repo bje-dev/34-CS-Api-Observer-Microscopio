309798754387c2d5cc0456110d6ba6196fbba83e
Bernardo Espinoza <bernardo.julian.espinoza@gmail.com>
2024-03-17 22:23:45 -0300

    Controller and Model

---
+++ API Microscopio/Controllers/HabitanteController.cs
@@ -0,0 +1,38 @@
+﻿using API_Microscopio.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Microscopio.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HabitanteController : ControllerBase
+    {
+        public static Random random = new Random();
+
+        [HttpGet]
+        [Route("habitante")]
+        public IActionResult GetHabitante() {
+
+            Habitante _habitante = default;
+
+            var nombre = new string[] { "Colembolos", "Acaros", "Tardigrados", "Nematodos", "Rotiferos", "Amebas", "Ciliados", "Flagelados", "Microalgas", "Hongos", "Bacterias" };
+            var color = new string[] { "Solido", "Traslucido" };
+            var tamaño = random.Next(10, 100);
+            var tipo = new string[] { "Fauna", "Flora" };
+
+            _habitante = new Habitante();
+
+            _habitante.Nombre = nombre[random.Next(nombre.Length)];
+            _habitante.Color = color[random.Next(color.Length)];
+            _habitante.Tamaño = tamaño;
+            _habitante.Tipo = tipo[random.Next(tipo.Length)];
+
+        return Ok(_habitante);
+        
+        }
+
+
+
+
+    }
+}
---
+++ API Microscopio/Models/Habitante.cs
@@ -0,0 +1,11 @@
+﻿namespace API_Microscopio.Models
+{
+    public class Habitante
+    {
+        public string Nombre { get; set; }
+        public string Color { get; set; }
+        public int Tamaño { get; set; }
+        public string Tipo { get; set; }
+
+    }
+}
